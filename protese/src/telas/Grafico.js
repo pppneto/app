8b5b0538635e955dc1539d06fdd18f65e4ae550e
marcus-soares <53491241+marcus-soares@users.noreply.github.com>
2019-07-30 18:19:33 -0300

    Grafico
    Adicionei grafico com dados constantes

--- protese/src/telas/Grafico.js
+++ protese/src/telas/Grafico.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Text ,TouchableOpacity } from 'react-native'
-
-
-class Grafico extends Component {
-
+import { LineChart, Grid } from 'react-native-svg-charts'
+ 
+class Grafico extends React.PureComponent {
+ 
     render() {
+ 
+        const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+ 
         return (
-            <View style={styles.mainView}>
-                <TouchableOpacity onPress = {() => this.props.navigation.navigate('Main')}>
-                <Text>{"GRAFICO AQUI"}</Text>
-                </TouchableOpacity>
-            </View>
+            <LineChart
+                style={{ height: 200 }}
+                data={ data }
+                svg={{ stroke: 'rgb(134, 65, 244)' }}
+                contentInset={{ top: 20, bottom: 20 }}
+            >
+                <Grid/>
+            </LineChart>
         )
     }
+ 
 }
+
 
 const styles = StyleSheet.create({
     mainView: {
