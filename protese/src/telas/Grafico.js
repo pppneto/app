ae16c776142ebf826e2c40d6218dde23da12c0fc
Márcio Filho <marciof1555@gmail.com>
2019-07-31 18:45:06 -0300

    rotação de tela

--- protese/src/telas/Grafico.js
+++ protese/src/telas/Grafico.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Text ,TouchableOpacity } from 'react-native'
 import { LineChart, Grid } from 'react-native-svg-charts'
+import Orientation from 'react-native-orientation'
  
 class Grafico extends React.PureComponent {
- 
+
+    componentDidMount() {
+        Orientation.lockToLandscape()
+    }
+    
+   
+
+   
+    
+   
     render() {
  
         const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
@@ -33,6 +43,8 @@ const styles = StyleSheet.create({
 
 Grafico.navigationOptions = {
     title: 'Gráfico'
+
+    
 }
 
 export default Grafico
