ad0d525699dfe64a6043a00a50fd69cd3073cabb
marcus-soares <53491241+marcus-soares@users.noreply.github.com>
2019-07-31 18:56:39 -0300

    Grafico atualiza com botão
    Grafico gera novos valores e reatualiza a renderização automaticamente

--- protese/src/telas/Grafico.js
+++ protese/src/telas/Grafico.js
@@ -1,22 +1,52 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Text ,TouchableOpacity } from 'react-native'
 import { LineChart, Grid } from 'react-native-svg-charts'
+
  
 class Grafico extends React.PureComponent {
  
+state = {
+    data: new Array( 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 )
+}
+
+addData = () => {
+    let data = this.state.data.slice()
+    let random = 0
+    
+    data.shift()
+    random = Math.random()*100
+    random = parseInt(random)
+    data.push(random)   
+
+    this.setState({data})
+}
+
     render() {
- 
-        const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+  
  
         return (
+            <View>
             <LineChart
                 style={{ height: 200 }}
-                data={ data }
+                data={ this.state.data }
                 svg={{ stroke: 'rgb(134, 65, 244)' }}
                 contentInset={{ top: 20, bottom: 20 }}
             >
                 <Grid/>
-            </LineChart>
+            </LineChart>    
+
+            <TouchableOpacity onPress = {() => this.addData()}>
+            <View style = {styles.b_grafico}>
+            
+            </View>
+            </TouchableOpacity>
+            <View>
+                <Text>
+                    {this.state.data}
+                </Text>
+            </View>
+            </View>
+            
         )
     }
  
@@ -27,8 +57,18 @@ const styles = StyleSheet.create({
     mainView: {
         flex:1,
         alignItems: 'center',
-        justifyContent: 'center'
-    }
+        justifyContent: 'center',borderRadius: 15
+        },
+    b_grafico:{
+        borderRadius: 20,
+        borderWidth: 1,
+        height: 40,
+        width: 40,
+        borderColor: 'red',
+        backgroundColor: 'gray'
+    }    
+
+    
 })
 
 Grafico.navigationOptions = {
