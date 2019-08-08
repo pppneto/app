a71415bc8cc79e8c0d378166f9590eea8ba24da0
Márcio Filho <marciof1555@gmail.com>
2019-08-08 19:25:51 -0300

    Update Main.js
    add os cinco botões para os dedo, quando pressionado muda para cor azul. Ainda não conseguimos ucupar a linha da tela toda.

--- protese/src/telas/Main.js
+++ protese/src/telas/Main.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet,Button, View, Text } from 'react-native'
 import ActionButton from 'react-native-action-button'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import Orientation from 'react-native-orientation'
 
 
 class Main extends Component  {
-
+    state={
+        polegar: false,
+        indicador: false,
+        medio: false,
+        anelar: false,
+        minimo: false
+    }
     componentDidMount(){
         const willFocus = this.props.navigation.addListener('willFocus', payload => this.componentWillFocus(payload))
     }
@@ -19,11 +25,71 @@ class Main extends Component  {
         willFocus.remove()
     }
     
+    clicaDedos(dedo){
 
+        if(dedo == 1){
+            this.setState({polegar:!this.state.polegar,texto:"polegar"})
+        }
+        else if(dedo == 2){
+            this.setState({indicador:!this.state.indicador,texto:"indicador"})
+
+        }
+        else if(dedo == 3){
+            this.setState({medio:!this.state.medio,texto:"medio"})
+
+        }
+        else if(dedo == 4){
+            this.setState({anelar:!this.state.anelar,texto:"anelar"})
+
+        }
+        else if(dedo == 5){
+            this.setState({minimo:!this.state.minimo,texto:"minimo"})
+
+        }
+
+    
+    }
     render() {
 
         return (
             <View style={styles.mainView}>
+                    <View style={styles.viewBotoes}>
+                        <Button
+                        onPress={() => 
+                        this.clicaDedos(1)
+                        }
+                        title="Polegar"
+                        color={this.state.polegar == true ? "#0000ff" : "#000000"}
+                        />
+                        <Button
+                         onPress={() => 
+                            this.clicaDedos(2)
+                            }
+                        title="Indicador"
+                        color={this.state.indicador == true ? "#0000ff" : "#000000"}
+                        />
+                        <Button 
+                         onPress={() => 
+                            this.clicaDedos(3)
+                            }
+                        title="Médio"
+                        color={this.state.medio == true ? "#0000ff" : "#000000"}
+                        />
+                        <Button
+                         onPress={() => 
+                            this.clicaDedos(4)
+                            }
+                        title="Anelar"
+                        color={this.state.anelar == true ? "#0000ff" : "#000000"}
+                        />
+                        <Button
+                         onPress={() => 
+                            this.clicaDedos(5)
+                            }
+                        title="Mínimo"
+                        color={this.state.minimo == true ? "#0000ff" : "#000000"}
+                        />
+                    </View>
 
                 <ActionButton>
 
@@ -51,7 +117,11 @@ const styles = StyleSheet.create({
         fontSize: 20,
         height: 22,
         color: 'white',
-    }
+    },
+    viewBotoes:{
+        flexDirection:'row',
+    
+    }    
 })
 
 Main.navigationOption = {
